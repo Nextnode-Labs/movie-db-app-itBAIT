12833de236a932a3b2f77d673c2192af14251639
Eugeniy <bait.new@gmail.com>
2021-11-09 15:03:52 +0200

    install Blueprint UI library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 function App() {
   return (
