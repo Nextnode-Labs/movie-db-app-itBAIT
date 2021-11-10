74c82a0e4b25cd1ee894a7945b4f0d08be233894
Eugeniy <bait.new@gmail.com>
2021-11-10 14:00:20 +0200

    heroImage component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 
 import Header from './components/Header'
+import Home from './components/Home'
 import { GlobalStyle } from './GlobalStyle'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      Start here. <GlobalStyle />
+      <Home />
+      <GlobalStyle />
     </div>
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+// config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
+// components
+import HeroImage from './HeroImage'
+
+// hooks
+import { useHomeFetch } from '../hooks/useHomeFetch'
+// images
+import NoImage from '../images/no_image.jpg'
+import { Toaster } from '@blueprintjs/core'
+
+const Home = () => {
+  const { state, loading, error } = useHomeFetch()
+  console.log(state)
+
+  return (
+    <>
+      {state.results[0] && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      )}
+    </>
+  )
+}
+
+export default Home
