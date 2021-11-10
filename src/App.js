796c2d91902f5b5af76eb63cc3d7daad45dfc707
Eugeniy <bait.new@gmail.com>
2021-11-10 12:03:14 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'normalize.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 
+import Header from './components/Header'
 import { GlobalStyle } from './GlobalStyle'
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       Start here. <GlobalStyle />
     </div>
   )
