1d9d576b6e52bb1045ca9a40e216463eb500982e
Eugeniy <bait.new@gmail.com>
2021-11-17 14:10:08 +0200

    add movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,3 +1,21 @@
-const Movie = () => <div>Movie</div>
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
+import Grid from './Grid'
+import SpinnerWrapper from './SpinnerWrapper'
+
+import { useMovieFetch } from '../hooks/useMovieFetch'
+
+import NoImage from '../images/no_image.jpg'
+
+import { useParams } from 'react-router'
+
+const Movie = () => {
+  const { movieID } = useParams()
+  const { state: movie, loading, error } = useMovieFetch(movieID)
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  )
+}
 
 export default Movie
