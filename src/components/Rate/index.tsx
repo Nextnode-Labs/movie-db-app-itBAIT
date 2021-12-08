a5928c9fc636ae9e3aa17580fbb6f3f0c1fe75cd
Eugeniy <bait.new@gmail.com>
2021-12-08 14:35:28 +0200

    rate component and logic

---
+++ src/components/Rate/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { callbackify } from 'util'
+import Button from '../Button'
+
+type Props = {
+  callback: (value: string) => void
+}
+
+const Rate: React.FC<Props> = ({ callback }) => {
+  const [value, setvalue] = useState('5')
+  return (
+    <div>
+      <input
+        type="range"
+        min="1"
+        max="10"
+        value={value}
+        onChange={(e) => setvalue(e.currentTarget.value)}
+      />
+      {value}
+      <p>
+        <Button text="Rate" callback={() => callback(value)} />
+      </p>
+    </div>
+  )
+}
+
+export default Rate
