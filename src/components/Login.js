01fbf0279a4e36edba557a92bd795f77410137b9
Eugeniy <bait.new@gmail.com>
2021-12-08 13:45:23 +0200

    login component logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,23 +3,50 @@ import API from '../API'
 import Button from './Button'
 import { Wrapper } from './Login.styles'
 import { Context } from '../context'
+import { useContext, useState } from 'react'
 
 const Login = () => {
-  const handleSubmit = (e) => {}
-  const handleInput = (e) => {}
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(false)
+
+  const [_user, setuser] = useContext(Context)
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    setError(false)
+    try {
+      const requestToken = await API.getRequestToken()
+      const sessionId = await API.authenticate(requestToken, userName, password)
+      setuser({ sessionId: sessionId.session_id, userName })
+      console.log(sessionId)
+      navigate('/')
+    } catch {
+      setError(true)
+    }
+  }
+
+  const handleInput = (e) => {
+    const name = e.currentTarget.name
+    const value = e.currentTarget.value
+
+    if (name === 'username') setUserName(value)
+    if (name === 'password') setPassword(value)
+  }
 
   return (
     <Wrapper>
+      {error && <div className="error">Something went wrong...</div>}
       <label>Username:</label>
       <input
         type="text"
-        value="state value"
+        value={userName}
         name="username"
         onChange={handleInput}
       />
       <input
         type="password"
-        value="state value"
+        value={password}
         name="password"
         onChange={handleInput}
       />
