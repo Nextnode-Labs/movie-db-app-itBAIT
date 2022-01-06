80f4ac937d7c2717f127aef22dde09d138ca14cd
Eugeniy <bait.new@gmail.com>
2022-01-06 16:51:48 +0200

    actor page layout

--- src/components/PersonInfo/PersonInfo.styles.ts
+++ src/components/PersonInfo/PersonInfo.styles.ts
@@ -1,55 +1,44 @@
 import styled from 'styled-components'
-import { IMAGE_BASE_URL, BACKDROP_SIZE } from '../../config'
-
 
 export const Wrapper = styled.div`
-
+  padding: 1rem 2rem;
 `
 
 export const Content = styled.div`
   display: flex;
   max-width: var(--maxWidth);
   margin: 0 auto;
-  background: rgba(0, 0, 0, 0.7);
-  border-radius: 20px;
 
   @media screen and (max-width: 768px) {
     display: block;
     max-height: none;
   }
+  .person-sidebar {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    color: var(--white);
+    gap: 1rem;
+    .person-category {
+      font-weight: 600;
+    }
+  }
 `
 
 export const Text = styled.div`
   width: 100%;
-  padding: 20px 40px;
+  padding: 0 40px;
   color: var(--white);
   overflow: hidden;
-
-  .rating-directors {
-    display: flex;
-    justify-content: flex-start;
+  @media screen and (max-width: 768px) {
+    padding: 0;
   }
-  .score {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 35px;
-    height: 35px;
-    background: #fff;
-    color: #000;
-    font-weight: 800;
-    border-radius: 50%;
-    margin: 0;
-  }
-  .director {
-    margin: 0 0 0 40px;
-    p {
-      margin: 0;
-    }
-  }
-  h1 {
-    @media screen and (max-width: 768px) {
-      font-size: var(--fontBig);
-    }
-  }
+`
+export const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 420px;
+  object-fit: cover;
+  border-radius: 15px;
+  align-self: center;
 `
--- src/components/PersonInfo/index.tsx
+++ src/components/PersonInfo/index.tsx
@@ -1,12 +1,8 @@
-import { Wrapper, Content, Text } from './PersonInfo.styles'
+import { Wrapper, Content, Text, Image } from './PersonInfo.styles'
 
-import Thumb from '../Thumb'
-import Rate from '../Rate'
-import Favorite from '../Favorite'
-import API, { Person } from '../../API'
+import { Person } from '../../API'
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config'
 import NoImage from '../../images/no_image.jpg'
-import Actor from '../Actor'
 // import { Context } from '../../context'
 // import { useContext } from 'react'
 
@@ -19,22 +15,55 @@ const PersonInfo: React.FC<Props> = ({ person }) => {
   // const handleRating = async (value: string) => {
   //   await API.rateMovie(user.sessionId, movie.id, parseInt(value))
   // }
+  const genders = ['Unknown', 'Female', 'Male']
+  var today = new Date()
+  const birthday = new Date(person.birthday)
+  let age = today.getFullYear() - birthday.getFullYear()
+  const m = today.getMonth() - birthday.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) age--
 
   return (
     <Wrapper>
       <Content>
-        <div>
-          <Actor
-            name={person.name}
-            id={person.id}
-            imageUrl={
+        <div className="person-sidebar">
+          <Image
+            src={
               person.profile_path
                 ? `${IMAGE_BASE_URL}${POSTER_SIZE}${person.profile_path}`
                 : NoImage
             }
           />
+          <h3>Personal Info</h3>
+          <p>
+            <span className="person-category">Known For</span>
+            <br />
+            <span>{person.known_for_department}</span>
+          </p>
+          <p>
+            <span className="person-category">Gender</span>
+            <br />
+            <span>{genders[person.gender]}</span>
+          </p>
+          <p>
+            <span className="person-category">Birthday</span>
+            <br />
+            <span className="person-category">
+              {`${person.birthday} (Age: ${age})`}
+            </span>
+          </p>
+          <p>
+            <span className="person-category">Place of Birth</span>
+            <br />
+            <span className="person-category">{person.place_of_birth}</span>
+          </p>
         </div>
-        <Text>ddd</Text>
+        <Text>
+          <h1>{person.name}</h1>
+          <h3>Biography</h3>
+          {person.biography.split('\n\n').map((p) => (
+            <p>{p}</p>
+          ))}
+        </Text>
       </Content>
     </Wrapper>
   )
